149f3a8c31f7e78560ed4a9311a867f354056d68
Robraym <robson.raymundo@gmail.com>
2026-05-25 10:20:41 -0300

    Refresh notification layout

--- app/src/main/java/ray/droid/com/droidcatchnotification/activity/MainActivity.java
+++ app/src/main/java/ray/droid/com/droidcatchnotification/activity/MainActivity.java
@@ -2,8 +2,10 @@ package ray.droid.com.droidcatchnotification.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import ray.droid.com.droidcatchnotification.R;
 import ray.droid.com.droidcatchnotification.common.DroidCommon;
@@ -11,19 +13,48 @@ import ray.droid.com.droidcatchnotification.gdrive.CreateFileActivity;
 
 public class MainActivity extends AppCompatActivity  {
     private Context context;
+    private TextView textStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        context = getBaseContext();
-        DroidCommon.ShowListener(context);
-        if (DroidCommon.GetDriveFile(context) == null)
-        {
-            Intent mIntent = new Intent(context, CreateFileActivity.class);
-            mIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(mIntent);
+        context = this;
+        textStatus = findViewById(R.id.textStatus);
+        findViewById(R.id.buttonNotifications).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DroidCommon.ShowListener(context);
+            }
+        });
+        findViewById(R.id.buttonDrive).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDriveSetup();
+            }
+        });
+        updateStatus();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateStatus();
+    }
+
+    private void updateStatus() {
+        if (textStatus == null) {
+            return;
         }
-        finish();
+        if (DroidCommon.GetDriveFile(context) == null) {
+            textStatus.setText(R.string.main_status_drive_missing);
+        } else {
+            textStatus.setText(R.string.main_status_ready);
+        }
+    }
+
+    private void openDriveSetup() {
+        Intent intent = new Intent(context, CreateFileActivity.class);
+        startActivity(intent);
     }
 }
