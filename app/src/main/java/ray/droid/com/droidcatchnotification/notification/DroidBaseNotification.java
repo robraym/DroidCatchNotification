6e4bc0f2624048b2d7840e1d79ef89be32595e3f
Robraym <robson.raymundo@gmail.com>
2026-05-26 19:31:38 -0300

    removido o metodo que cancelava as notificacoes

--- app/src/main/java/ray/droid/com/droidcatchnotification/notification/DroidBaseNotification.java
+++ app/src/main/java/ray/droid/com/droidcatchnotification/notification/DroidBaseNotification.java
@@ -14,7 +14,6 @@ public class DroidBaseNotification extends NotificationListenerService {
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
-        cancelAllNotifications();
         super.onNotificationPosted(sbn);
     }
 
@@ -25,7 +24,6 @@ public class DroidBaseNotification extends NotificationListenerService {
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn, RankingMap rankingMap) {
-        cancelAllNotifications();
         super.onNotificationPosted(sbn, rankingMap);
     }
 
